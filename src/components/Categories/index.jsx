1639360c0fae12e5e129c0234426bb9955c59ff1
Lucas Dzin Pedroso <lucasdzin@gmail.com>
2022-09-10 11:22:17 -0300

    fix: alteração da requisição para axios

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import api from "../../services/api";
 
 import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
@@ -23,16 +24,9 @@ export default function Categories() {
     const filteredCategories = categoryFilter.length > 0 ? categories.filter(category => categoryFilter.includes(category.tipo)) : categories;
 
     useEffect(() => {
-        fetch(`https://gama-academy-api.herokuapp.com/categoria`)
-            .then(resp => {
-                resp.json()
-                    .then(data => {
-                        setCategories(data);
-                    });
-            })
-            .catch(err => {
-                console.log(`Erro: ${err}`);
-            });
+        api.get('/categoria').then(resp => {
+            setCategories(resp.data);
+        });
     }, []);
 
     const addCategory = () => {
@@ -56,7 +50,7 @@ export default function Categories() {
                 gap: "1rem",
             }}>
                 <Typography component="h3" sx={{
-                    fontSize: "1.5rem",
+                    fontSize: "1.2rem",
                     fontWeight: "bold",
                     color: "#2D3748",
                 }}>Categorias</Typography>
@@ -68,40 +62,52 @@ export default function Categories() {
                     gap: "1rem",
                 }}>
                     <FormControlLabel sx={{
-                        margin: 0,
+                        margin: "0",
                     }} control={<Switch size="medium" onChange={ (e) => { e.target.checked ? setCategoryFilter([...categoryFilter, "receita"]) : setCategoryFilter(categoryFilter.filter( filter => filter !== "receita" )) } } />} label="Entrada" />
                     <FormControlLabel sx={{
-                        margin: 0,
+                        margin: "0",
                     }} control={<Switch size="medium" onChange={ (e) => { e.target.checked ? setCategoryFilter([...categoryFilter, "despesa"]) : setCategoryFilter(categoryFilter.filter( filter => filter !== "despesa" )) } } />} label="Saída" />
                 </FormGroup>
             </Grid>
 
             <List sx={{
-                width: "100%"
+                width: "100%",
+                height: "15rem",
+                overflowY: "scroll",
+                paddingTop: "0",
+                paddingBottom: "0",
+                marginTop: "0.8rem",
+                marginBottom: "0.8rem",
             }}>
             {
                 filteredCategories.map( category => (
                     <ListItem key={category.id} sx={{
+                        paddingTop: "0.2rem",
+                        paddingBottom: "0.2rem",
                         paddingLeft: "0",
                         paddingRight: "0",
                     }}>
                         <ListItemIcon sx={{
                             minWidth: "0",
                             marginRight: "0.5rem",
-                        }} >
+                        }}>
                             {
                                 category.tipo === "despesa" ?
-                                    <ArrowCircleDownOutlinedIcon fontSize="medium" sx={{ color: "#E53E3E" }} />
+                                    <ArrowCircleDownOutlinedIcon fontSize="medium" sx={{ color: "#E53E3E" }}/>
                                 :
-                                    <ArrowCircleUpOutlinedIcon fontSize="medium" sx={{ color: "#48BB78" }} />
+                                    <ArrowCircleUpOutlinedIcon fontSize="medium" sx={{ color: "#48BB78" }}/>
                             }
                         </ListItemIcon>
                         <ListItemText primary={category.nome} />
                         <IconButton aria-label="edit" onClick={ () => editCategory( category.id ) }>
-                            <EditOutlinedIcon fontSize="medium" sx={{ color: '#000000' }} />
+                            <EditOutlinedIcon fontSize="medium" sx={{
+                                color: "#000000",
+                            }}/>
                         </IconButton>
                         <IconButton aria-label="delete" onClick={ () => deleteCategory( category.id ) }>
-                            <DeleteForeverOutlinedIcon fontSize="medium" sx={{ color: '#000000' }} />
+                            <DeleteForeverOutlinedIcon fontSize="medium" sx={{
+                                color: "#000000",
+                            }}/>
                         </IconButton>
                     </ListItem>     
                 ))
