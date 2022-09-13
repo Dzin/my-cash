0dfe575982fb2e9a05e2c8ac5adcd60309fc03d8
Vinicius BF <vini91bf@gmail.com>
2022-09-13 14:23:10 -0300

    feat: change transactionList state and get transactions useEffect to Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,33 @@
 import React from "react";
-import { Transactions } from "../../components/Transactions";
+import { useState, useEffect } from "react";
+import { Transactions } from "../../components/TransactionListingCard";
+import api from "../../services/api";
 
 export default function Home() {
-  return <Transactions />;
+  const [transactionList, setTransactionList] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/transacao")
+      .then((resp) => {
+        setTransactionList(resp.data);
+      })
+      .catch((err) => {
+        console.error("Can not get Transaction List!", `Error: ${err}`);
+      });
+  }, []);
+
+  const handleEditTransaction = function (id, e) {
+    console.log("Edit transaction");
+  };
+
+  const handleDeleteTransaction = function (id, e) {
+    console.log("Delete transaction");
+  };
+
+  const handleAddNewTransaction = function (transaction, e) {
+    console.log("Add new transaction");
+  };
+
+  return <Transactions transactionList={transactionList} />;
 }
