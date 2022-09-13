7d6f71f04d67d03b939bed7bcad590397d3fea27
Vinicius BF <vini91bf@gmail.com>
2022-09-12 23:53:52 -0300

    feat: implement ToggleType component

---
+++ src/components/ToggleType/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+import { ToggleButtonGroup, ToggleButton } from "@mui/material";
+
+export const ToggleType = ({ handleToggleType }) => {
+  const [type, setType] = useState("");
+
+  return (
+    <ToggleButtonGroup
+      color="primary"
+      value={type}
+      exclusive
+      onChange={(e, newValue) => {
+        setType(newValue);
+      }}
+      aria-label="Platform"
+    >
+      <ToggleButton value="entrada" onClick={handleToggleType}>
+        Entrada
+      </ToggleButton>
+      <ToggleButton value="saida" onClick={handleToggleType}>
+        Saída
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+};
