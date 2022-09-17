44d3c08711083ca356f1ac424f2a6a60db768e9c
Vinicius BF <vini91bf@gmail.com>
2022-09-17 05:27:26 -0300

    feat: add list component to TransactionListingCard

--- src/components/TransactionListingCard/index.jsx
+++ src/components/TransactionListingCard/index.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { useState } from "react";
 
-import { Grid, Typography, Button } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+  Button,
+} from "@mui/material";
 
 import { DateInput } from "../DateInput";
 import { ToggleType } from "../ToggleType";
+
+import ArrowCircleUpOutlinedIcon from "@mui/icons-material/ArrowCircleUpOutlined";
+import ArrowCircleDownOutlinedIcon from "@mui/icons-material/ArrowCircleDownOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
 
 export const TransactionListingCard = ({ transactionList }) => {
   const [date, setDate] = useState(null);
@@ -61,12 +75,99 @@ export const TransactionListingCard = ({ transactionList }) => {
       </Typography>
       <DateInput handleSelectDate={handleSelectDate} />
       <ToggleType handleToggleType={handleToggleType} />
+      <List
+        sx={{
+          width: "100%",
+          height: "15rem",
+          overflowY: "scroll",
+          paddingTop: "0",
+          paddingBottom: "0",
+          marginTop: "1rem",
+          marginBottom: "1rem",
+          "&::-webkit-scrollbar": {
+            width: 5,
+          },
+          "&::-webkit-scrollbar-track": {
+            backgroundColor: "#F8F9Fa",
+            borderRadius: "1.2rem",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "transparent",
+            backgroundImage:
+              "linear-gradient(136.64deg, #658DD1 0%, #2D3748 100%)",
+            borderRadius: "1.2rem",
+          },
+        }}
+      >
+        {listFilteredTransactions().map((transaction) => (
+          <ListItem
+            key={transaction.id}
+            sx={{
+              paddingTop: "0.2rem",
+              paddingBottom: "0.2rem",
+              paddingLeft: "0",
+              paddingRight: "0",
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: "0",
+                marginRight: "0.5rem",
+              }}
+            >
+              {transaction.tipo === "despesa" ? (
+                <ArrowCircleDownOutlinedIcon
+                  fontSize="small"
+                  sx={{
+                    color: "#E53E3E",
+                  }}
+                />
+              ) : (
+                <ArrowCircleUpOutlinedIcon
+                  fontSize="small"
+                  sx={{
+                    color: "#48BB78",
+                  }}
+                />
+              )}
+            </ListItemIcon>
+            <ListItemText
+              primary={transaction.descricao}
+              primaryTypographyProps={{
+                fontSize: {
+                  md: "0.9rem",
+                  xs: "0.8rem",
+                },
+              }}
+            />
+            <IconButton aria-label="edit" onClick={() => console.log("Edit")}>
+              <EditOutlinedIcon
+                fontSize="small"
+                sx={{
+                  color: "#000000",
+                }}
+              />
+            </IconButton>
+            <IconButton
+              aria-label="delete"
+              onClick={() => console.log("delete")}
+            >
+              <DeleteForeverOutlinedIcon
+                fontSize="small"
+                sx={{
+                  color: "#000000",
+                }}
+              />
+            </IconButton>
+          </ListItem>
+        ))}
+      </List>
+      <Button variant="contained">Adicionar</Button>
       <ul>
         {listFilteredTransactions().map((item) => (
           <li key={item.id}>{item.id}</li>
         ))}
       </ul>
-      <Button variant="contained">Adicionar</Button>
     </Grid>
   );
 };
