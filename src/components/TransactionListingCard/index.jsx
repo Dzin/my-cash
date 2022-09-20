0626e0065e11815e609fce6dd44a670be0ffbc36
Vinicius BF <vini91bf@gmail.com>
2022-09-20 02:15:05 -0300

    feat: refactor TransactionListingCard  and implement loading after delete transaction

--- src/components/TransactionListingCard/index.jsx
+++ src/components/TransactionListingCard/index.jsx
@@ -12,17 +12,30 @@ import {
   Button,
 } from "@mui/material";
 
-import { DateInput } from "../DateInput";
-import { ToggleType } from "../ToggleType";
-
 import ArrowCircleUpOutlinedIcon from "@mui/icons-material/ArrowCircleUpOutlined";
 import ArrowCircleDownOutlinedIcon from "@mui/icons-material/ArrowCircleDownOutlined";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
 
-export const TransactionListingCard = ({ transactions }) => {
+import TransactionRegistration from "../modal/TransactionRegistration";
+import Loading from "../Loading";
+import { DateInput } from "../DateInput";
+import { ToggleType } from "../ToggleType";
+
+import api from "../../services/api";
+
+import { toast } from "react-toastify";
+
+export const TransactionListingCard = ({
+  transactions,
+  setTransactions,
+  loading,
+  setLoading,
+}) => {
   const [date, setDate] = useState(null);
   const [type, setType] = useState("");
+  const [openCreateTransactionModal, setOpenCreateTransactionModal] =
+    useState(false);
 
   const filterTransactionsByDate = (transaction) => {
     const currentTransactionDate = new Date(transaction.data);
@@ -37,7 +50,7 @@ export const TransactionListingCard = ({ transactions }) => {
   };
 
   const filterTransactionsByType = (transaction) => {
-    return transaction.categoria.tipo === type;
+    return transaction.tipo === type;
   };
 
   const listFilteredTransactions = () => {
@@ -57,112 +70,196 @@ export const TransactionListingCard = ({ transactions }) => {
     setType(selectedValue || "");
   };
 
+  const handleEditTransaction = (id) => {
+    console.log(id);
+    // Abre modal de editar
+  };
+
+  const handleDeleteTransaction = (id) => {
+    setLoading(true);
+
+    api
+      .delete(`/transacao/${id}`)
+      .then(() => {
+        toast.success("Transação deletada com sucesso");
+        setTransactions(
+          transactions.filter((transaction) => transaction.id !== id)
+        );
+        setLoading(false);
+      })
+      .catch((error) => {
+        toast.error("Não foi possível deletar a transação");
+        setLoading(false);
+      });
+  };
+
+  const handleAddNewTransaction = () => {
+    setOpenCreateTransactionModal(true);
+  };
+
   return (
-    <Grid>
-      <Typography
-        component="h3"
-        fontWeight="700"
-        fontSize={{
-          xs: "1.2rem",
+    <>
+      <Grid
+        padding={{
+          md: "1.2rem 1.5rem",
+          xs: "1.5rem 1.2rem",
         }}
-        textAlign={{
-          sm: "left",
-          xs: "center",
-        }}
-        color="#2D3748"
       >
-        Transações
-      </Typography>
-      <DateInput handleSelectDate={handleSelectDate} />
-      <ToggleType handleToggleType={handleToggleType} />
-      <List
-        sx={{
-          width: "100%",
-          height: "15rem",
-          overflowY: "scroll",
-          paddingTop: "0",
-          paddingBottom: "0",
-          marginTop: "1rem",
-          marginBottom: "1rem",
-          "&::-webkit-scrollbar": {
-            width: 5,
-          },
-          "&::-webkit-scrollbar-track": {
-            backgroundColor: "#F8F9Fa",
-            borderRadius: "1.2rem",
-          },
-          "&::-webkit-scrollbar-thumb": {
+        <Grid
+          sx={{
+            display: "flex",
+            flexDirection: {
+              sm: "row",
+              xs: "column",
+            },
+            alignItems: "center",
+            justifyContent: {
+              sm: "space-between",
+              xs: "center",
+            },
+            gap: {
+              sm: "1rem",
+              xs: "0.5rem",
+            },
+          }}
+        >
+          <Typography
+            component="h3"
+            fontWeight="700"
+            fontSize={{
+              xs: "1.2rem",
+            }}
+            textAlign={{
+              sm: "left",
+              xs: "center",
+            }}
+            color="#2D3748"
+          >
+            Transações
+          </Typography>
+          <DateInput handleSelectDate={handleSelectDate} />
+          <ToggleType handleToggleType={handleToggleType} />
+        </Grid>
+
+        <List
+          sx={{
+            width: "100%",
+            height: "15rem",
+            overflowY: "scroll",
+            paddingTop: "0",
+            paddingBottom: "0",
+            marginTop: "1rem",
+            marginBottom: "1rem",
+            "&::-webkit-scrollbar": {
+              width: 5,
+            },
+            "&::-webkit-scrollbar-track": {
+              backgroundColor: "#F8F9Fa",
+              borderRadius: "1.2rem",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "transparent",
+              backgroundImage:
+                "linear-gradient(136.64deg, #658DD1 0%, #2D3748 100%)",
+              borderRadius: "1.2rem",
+            },
+          }}
+        >
+          {loading ? (
+            <Loading />
+          ) : (
+            listFilteredTransactions().map((transaction) => (
+              <ListItem
+                key={transaction.id}
+                sx={{
+                  paddingTop: "0.2rem",
+                  paddingBottom: "0.2rem",
+                  paddingLeft: "0",
+                  paddingRight: "0",
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: "0",
+                    marginRight: "0.5rem",
+                  }}
+                >
+                  {transaction.tipo === "despesa" ? (
+                    <ArrowCircleDownOutlinedIcon
+                      fontSize="small"
+                      sx={{
+                        color: "#E53E3E",
+                      }}
+                    />
+                  ) : (
+                    <ArrowCircleUpOutlinedIcon
+                      fontSize="small"
+                      sx={{
+                        color: "#48BB78",
+                      }}
+                    />
+                  )}
+                </ListItemIcon>
+                <ListItemText
+                  primary={transaction.descricao}
+                  primaryTypographyProps={{
+                    fontSize: {
+                      md: "0.9rem",
+                      xs: "0.8rem",
+                    },
+                  }}
+                />
+                <IconButton
+                  aria-label="edit"
+                  onClick={() => handleEditTransaction(transaction.id)}
+                >
+                  <EditOutlinedIcon
+                    fontSize="small"
+                    sx={{
+                      color: "#000000",
+                    }}
+                  />
+                </IconButton>
+                <IconButton
+                  aria-label="delete"
+                  onClick={() => handleDeleteTransaction(transaction.id)}
+                >
+                  <DeleteForeverOutlinedIcon
+                    fontSize="small"
+                    sx={{
+                      color: "#000000",
+                    }}
+                  />
+                </IconButton>
+              </ListItem>
+            ))
+          )}
+        </List>
+
+        <Button
+          variant="contained"
+          sx={{
+            fontSize: {
+              xs: "0.8rem",
+            },
+            fontWeight: "500",
             backgroundColor: "transparent",
             backgroundImage:
-              "linear-gradient(136.64deg, #658DD1 0%, #2D3748 100%)",
-            borderRadius: "1.2rem",
-          },
-        }}
-      >
-        {listFilteredTransactions().map((transaction) => (
-          <ListItem
-            key={transaction.id}
-            sx={{
-              paddingTop: "0.2rem",
-              paddingBottom: "0.2rem",
-              paddingLeft: "0",
-              paddingRight: "0",
-            }}
-          >
-            <ListItemIcon
-              sx={{
-                minWidth: "0",
-                marginRight: "0.5rem",
-              }}
-            >
-              {transaction.categoria.tipo === "despesa" ? (
-                <ArrowCircleDownOutlinedIcon
-                  fontSize="small"
-                  sx={{
-                    color: "#E53E3E",
-                  }}
-                />
-              ) : (
-                <ArrowCircleUpOutlinedIcon
-                  fontSize="small"
-                  sx={{
-                    color: "#48BB78",
-                  }}
-                />
-              )}
-            </ListItemIcon>
-            <ListItemText
-              primary={transaction.descricao}
-              primaryTypographyProps={{
-                fontSize: {
-                  md: "0.9rem",
-                  xs: "0.8rem",
-                },
-              }}
-            />
-            <IconButton aria-label="edit" onClick={() => console.log("Edit")}>
-              <EditOutlinedIcon
-                fontSize="small"
-                sx={{
-                  color: "#000000",
-                }}
-              />
-            </IconButton>
-            <IconButton
-              aria-label="delete"
-              onClick={() => console.log("delete")}
-            >
-              <DeleteForeverOutlinedIcon
-                fontSize="small"
-                sx={{
-                  color: "#000000",
-                }}
-              />
-            </IconButton>
-          </ListItem>
-        ))}
-      </List>
-      <Button variant="contained">Adicionar</Button>
-    </Grid>
+              "linear-gradient(136.64deg, #658DD1 1.59%, #2D3748 98.89%)",
+            padding: "0.4rem 2rem",
+            textTransform: "none",
+            borderRadius: "0.5rem",
+          }}
+          onClick={() => handleAddNewTransaction()}
+        >
+          Adicionar
+        </Button>
+      </Grid>
+
+      {/* <TransactionRegistration
+        open={openCreateTransactionModal}
+        setOpen={setOpenCreateTransactionModal}
+      /> */}
+    </>
   );
 };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 // MUI
-import { Container, Typography, Grid, Paper, Box } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 // IMGS
 import Logo from "../../assets/imgs/logo.png";
-// ICONS
-
 // STYLES
 import {
   BackgroundHeaderImage,
@@ -13,8 +11,9 @@ import {
 } from "./style";
 // SERVICES
 import api from "../../services/api";
+// HELPERS
+import { toast } from "react-toastify";
 //COMPONENTS
-import TransactionRegistration from "../../components/modal/TransactionRegistration";
 import { TransactionListingCard } from "../../components/TransactionListingCard";
 import Categories from "../../components/Categories";
 import CardTop from "./components/CardTop";
@@ -26,45 +25,58 @@ export default function Home() {
   const [loadingRegister, setLoadingRegister] = useState(false);
   const [categories, setCategories] = useState([]);
   const [loadingCategories, setLoadingCategories] = useState(false);
-  const [openRegister, setOpenRegister] = useState(false);
 
-  useEffect(() => {
-    setLoadingCategories(true);
-    api.get("/categoria").then((res) => {
-      setCategories(res.data);
-      setLoadingCategories(false);
-    });
-  }, []);
+  /* useEffect(() => {
+    setLoadingRegister(true);
 
-  useEffect(() => {
-    if (!openRegister) {
-      setLoadingRegister(true);
-
-      api
-        .get("/transacao")
-        .then((res) => {
-          setTransactions(res.data);
-          setLoadingRegister(false);
-        })
-        .catch((err) => {
-          console.error("Can not get Transaction List!", `Error: ${err}`);
-        });
-    }
-  }, [openRegister]);
-
-  const handleDeleteTransaction = function (id, e) {
     api
-      .delete(`/transacao/${id}`)
-      .then(() => {
-        alert(`Transação ${id} deletada com sucesso`);
-        setTransactions(
-          transactions.filter((transaction) => transaction.id !== id)
-        );
+      .get("/transacao")
+      .then((res) => {
+        setTransactions(res.data);
+        setLoadingRegister(false);
       })
-      .catch((err) => {
-        console.error(`Can not delete Transaction ${id}!`, `Error: ${err}`);
+      .catch((error) => {
+        console.error(error.message);
+        toast.error("Não foi possível carregar a lista de transações");
       });
-  };
+  }, []); */
+
+  /* useEffect(() => {
+    setLoadingCategories(true);
+
+    api
+      .get("/categoria")
+      .then((res) => {
+        setCategories(res.data);
+        setLoadingCategories(false);
+      })
+      .catch((error) => {
+        console.error(error.message);
+        toast.error("Não foi possível carregar a lista de categorias");
+      });
+  }, []); */
+
+  /* useEffect(() => {
+    if (!openRegister) {}
+  }, [openRegister]); */
+
+  useEffect(() => {
+    setLoadingRegister(true);
+    setLoadingCategories(true);
+
+    Promise.all([api.get("/transacao"), api.get("/categoria")])
+      .then((results) => {
+        setTransactions(results[0].data);
+        setLoadingRegister(false);
+        setCategories(results[1].data);
+        setLoadingCategories(false);
+      })
+      .catch((error) => {
+        toast.error("Não foi possível carregar os dados das listas");
+        setLoadingRegister(false);
+        setLoadingCategories(false);
+      });
+  }, []);
 
   return (
     <Container
@@ -90,38 +102,43 @@ export default function Home() {
             alignItems={"center"}
             spacing={2}
           >
-            <CardTop
+            {/* <CardTop
               type="Receitas"
               transactions={transactions}
               loading={loadingRegister}
               sm={6}
               xs={12}
               md={4}
-            />
+            /> */}
 
-            <CardTop
+            {/* <CardTop
               type="Despesas"
               transactions={transactions}
               loading={loadingRegister}
               sm={6}
               xs={12}
               md={4}
-            />
+            /> */}
 
-            <CardTop
+            {/* <CardTop
               type="Balanço"
               transactions={transactions}
               loading={loadingRegister}
               sm={12}
               xs={12}
               md={4}
-            />
+            /> */}
           </Grid>
         </Grid>
         <Grid item width={"100%"}>
           <Grid container spacing={2} justifyContent="center">
             <CardBotton xs={12} md={6}>
-              <TransactionListingCard transactions={transactions} />
+              <TransactionListingCard
+                transactions={transactions}
+                setTransactions={setTransactions}
+                loading={loadingRegister}
+                setLoading={setLoadingRegister}
+              />
             </CardBotton>
             <CardBotton xs={12} md={6}>
               <Categories
@@ -134,7 +151,6 @@ export default function Home() {
         </Grid>
       </GridFullContent>
       <Copyrights />
-      <TransactionRegistration open={openRegister} setOpen={setOpenRegister} />
     </Container>
   );
 }
